bf52ccc4b1a1d7d273eb052c73134969f0da2115
fxlwolcken <luke.wolckenhauer@foxnews.com>
2021-01-15 11:55:38 -0500

    environment var setup based on native dotenv

--- vue-three/src/components/HelloWorld.vue
+++ vue-three/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ msg }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +36,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    title() {
+      return process.env.VUE_APP_TITLE
+    }
   }
 }
 </script>
