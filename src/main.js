3e3de2d7af32e7ff98646db72bd85c237296e899
fxlwolcken <luke.wolckenhauer@foxnews.com>
2021-01-18 14:06:45 -0500

    begin styling with vuetify

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { store } from './store/store'
 import VueCookies from 'vue-cookies'
 import VModal from 'vue-js-modal'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
@@ -13,5 +14,6 @@ Vue.use(VueCookies)
 new Vue({
   render: h => h(App),
   router,
+  vuetify,
   store
 }).$mount('#app')
